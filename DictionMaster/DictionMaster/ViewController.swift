d2251f42383f3ee1af4d27a034a1fba426aee453
LucasGomeszx <lgomeszx@gmail.com>
2024-02-03 10:41:59 -0300

    Criando projeto.

---
+++ DictionMaster/DictionMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionMaster
+//
+//  Created by Lucas Gomesx on 03/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
